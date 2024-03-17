cb19492a8ee43acc28e9c0afec3b5d93d626a86e
aditya Singh <aditya9135853896@gmail.com>
2024-03-17 11:47:04 +0530

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   <main>
     <h1 className=""> Welcome To Krishnvi Public School</h1>
     <h1>We Will Be Comming Soon...</h1>
-    <Image src= {Invitation} alt="invitation" height={1280} width={720}/>
+    <Image src= "/static/inviation.png" alt="invitation" height={1280} width={720}/>
   </main>
 </>
  
